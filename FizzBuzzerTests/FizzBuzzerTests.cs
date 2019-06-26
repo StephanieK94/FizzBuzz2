baecf6498e14b24df8f3ba2bc7a2fa76cbaecad0
StephanieK <stephanie.kitchen@serko.com>
2019-06-26 13:57:41 +1200

    Creating a more generic test for defaults to regate duplication for all default numbers

--- FizzBuzzerTests/FizzBuzzerTests.cs
+++ FizzBuzzerTests/FizzBuzzerTests.cs
@@ -12,26 +12,12 @@ namespace FizzBuzzerTests.Library
     public class FizzBuzzerTests
     {
         [Test]
-        public void Buzzer_When1_Returns1()
+        public void Buzzer_WhenDefault_ReturnsInput(
+            [Values(2)] int input)
         {
-            //Arrange
-            int input = 1;
-
-            // Act
             string output = FizzBuzzer.GetValue(input);
 
-            // Assert
-            Assert.AreEqual("1", output);
-        }
-
-        [Test]
-        public void Buzzer_When2_Returns2()
-        {
-            int input = 2;
-
-            string output = FizzBuzzer.GetValue(input);
-
-            Assert.AreEqual("2", output);
+            Assert.AreEqual(input.ToString(), output);
         }
 
         [Test]

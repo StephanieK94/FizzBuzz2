fc5dc55a9489509223972f78aac842522f6b98c5
StephanieK <stephanie.kitchen@serko.com>
2019-06-26 13:37:24 +1200

    First commit after setting up the projects

---
+++ Fizzbuzz/FizzBuzzer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizzbuzz.Library
+{
+    public class FizzBuzzer
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1; i<=100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

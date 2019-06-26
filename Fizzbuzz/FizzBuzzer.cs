0e85a634114c3416aa2942c985f9ea5b523cb06a
StephanieK <stephanie.kitchen@serko.com>
2019-06-26 14:13:18 +1200

    Refactoring to remove duplication of checking for the divisibles twice in method

--- Fizzbuzz/FizzBuzzer.cs
+++ Fizzbuzz/FizzBuzzer.cs
@@ -10,11 +10,13 @@ namespace Fizzbuzz.Library
     {
         public static string GetValue(int input)
         {
-            if (input % 3 == 0 && input % 5 == 0) return "FizzBuzz";
-            if (input % 3 == 0) return "Fizz";
-            if (input % 5 == 0) return "Buzz";
+            string output = string.Empty;
 
-            return input.ToString();
+            if (input % 3 == 0) output += "Fizz";
+            if (input % 5 == 0) output += "Buzz";
+            if (string.IsNullOrEmpty(output)) output = input.ToString();
+
+            return output;
         }
     }
 }

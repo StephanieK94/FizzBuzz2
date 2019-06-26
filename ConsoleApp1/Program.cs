1e48bf3264b80a25dbf879bc8eef94527b051454
StephanieK <stephanie.kitchen@serko.com>
2019-06-26 14:18:13 +1200

    Some refactoring and addition of the FizzBuzzer into the Program

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fizzbuzz.Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,10 +13,8 @@ namespace FizzBuzz
         {
             for(int i=1; i<=100;i++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(FizzBuzzer.GetValue(i));
             }
-
-            Console.ReadLine();
         }
     }
 }
